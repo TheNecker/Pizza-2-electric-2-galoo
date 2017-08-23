9c4cedd952a7551e7faf2263573ab5f10debf32a
TheNecker <chrisp132012@gmail.com>
2017-08-23 15:49:43 -0400

    kinda works, no second page

--- app.rb
+++ app.rb
@@ -6,15 +6,19 @@ get '/' do
 end 
 
 post '/components' do 
-	crust = params[:cchoices]	
-	cheese = params[:chchoices]
-	meats = params[:mchoices]
-	veggies = params[:vchoices]
+	session[:cchoices] = params[:crust]	
+	session[:chchoices] = params[:cheese]
+	session[:mchoices] = params[:meats]
+	session[:vchoices] = params[:veggies]
+	puts "MADE IT TO POST COMPONENTS #{session[:cchoices]}"
+	puts "MADE IT TO POST COMPONENTS #{session[:chchoices]}"
+	puts "MADE IT TO POST COMPONENTS #{session[:mchoices]}"
+	puts "MADE IT TO POST COMPONENTS #{session[:vchoices]}"
 	redirect '/confirm'
 end 
 
 get '/confirm' do 
-	erb :confirm, locals: {cchoices: :crust, chchoices: :cheese, mchoices: :meats, vchoices: :veggies}
+	erb :confirm, locals: {crust: session[:cchoices], cheese: session[:chchoices], meats: session[:mchoices], veggies: session[:vchoices]}
 end 
 
 post '/confirm' do 
