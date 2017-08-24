1b896c5426251048607a003362d4ae7928d855cb
TheNecker <chrisp132012@gmail.com>
2017-08-24 11:16:10 -0400

    beyond half way

--- app.rb
+++ app.rb
@@ -11,16 +11,22 @@ post '/components' do
 	session[:mchoices] = params[:meats]
 	session[:vchoices] = params[:veggies]
 	session[:carchoices] = params[:carry]
+	session[:achoices] = params[:address]
+	session[:schoices] = params[:sauce]
+	session[:sichoices] = params[:size]
 	puts "MADE IT TO POST COMPONENTS #{session[:cchoices]}"
 	puts "MADE IT TO POST COMPONENTS #{session[:chchoices]}"
 	puts "MADE IT TO POST COMPONENTS #{session[:mchoices]}"
 	puts "MADE IT TO POST COMPONENTS #{session[:vchoices]}"
 	puts "MADE IT TO POST COMPONENTS #{session[:carchoices]}"
+	puts "MADE IT TO POST COMPONENTS #{session[:achoices]}"
+	puts "MADE IT TO POST COMPONENTS #{session[:schoices]}"
+	puts "MADE IT TO POST COMPONENTS #{session[:sichoices]}"
 	redirect '/confirm'
 end 
 
 get '/confirm' do 	
-	erb :confirm, locals: {crust: session[:cchoices], cheese: session[:chchoices], meats: session[:mchoices], veggies: session[:vchoices], carry: session[:carchoices]}
+	erb :confirm, locals: {crust: session[:cchoices], cheese: session[:chchoices], meats: session[:mchoices], veggies: session[:vchoices], carry: session[:carchoices], address: session[:achoices]}
 end 
 
 post '/confirm' do 
